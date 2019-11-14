99e4bcfbbee6e0c10cf7bc294630b435f3ee25df
Pavel Janko <jankop@volny.cz>
2019-11-14 19:14:43 +0100

    For Differential Input Configuration AD7747,  AD7745/46 ready

---
+++ AD774X_LOOP_47_02/AD774X_Sampling.ino
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------
+//conversion of the obtained data to real values
+//----------------------------------------------------------------------
+void ConvertData(void) {
+  long CapacitanceRaw = ((long)RTxBuff[ADR_CAP_DATAH] << 16) + ((long)RTxBuff[ADR_CAP_DATAM] << 8) + (long)RTxBuff[ADR_CAP_DATAL] - 0x800000;
+  // the next row is for AD7747 only    !!!!!!!!!!!!!!!!!!!
+  Capacitance = (double)CapacitanceRaw / 1024.0 / 1000.0;
+  // the next row is for AD7745/46 only !!!!!!!!!!!!!!!!!!!
+  //Capacitance = (double)CapacitanceRaw / 2048.0 / 1000.0;
+  unsigned long TemperatureRaw = ((unsigned long)RTxBuff[ADR_VT_DATAH] << 16) + ((unsigned long)RTxBuff[ADR_VT_DATAM] << 8) + (unsigned long)RTxBuff[ADR_VT_DATAL];
+  Temperature = (double) TemperatureRaw / 2048.0 - 4096.0;
+  Serial.print(F("\r\nCapacitance: "));
+  Serial.print(Capacitance, 6);
+  Serial.print(F(" pF"));
+  Serial.print(F("     Temperature: "));
+  Serial.print(Temperature, 1);
+  Serial.print(F(" deg.C"));
+}
+//----------------------------------------------------------------------
+// reading six data registers each sampling period,
+// specified by the SamplePeriod variable, and start a new conversion
+//----------------------------------------------------------------------
+void PeriodicSampling(void) {
+  if ((millis() - OldTime) > SamplePeriod) {
+    OldTime = millis();
+    // reading valid data
+    AD774X_Read_Registers(ADR_CAP_DATAH, RTxBuff, 6);
+    // start a new conversion
+    AD774X_Write_Single_Register(ADR_CFG, AD774X_Read_Single_Register(ADR_CFG) | SINGLE_SAMPLE);
+    // conversion of acquired data
+    ConvertData();
+  }
+}
